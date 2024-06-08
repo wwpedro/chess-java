50a00bb3d941714ae6a40e0d0ee985d3d529ba31
wwpedro <pedroaugusto.ifal@gmail.com>
2024-06-08 15:01:44 -0300

    BoardException and defensive programming

--- src/boadrgame/Board.java
+++ src/boadrgame/Board.java
@@ -7,7 +7,9 @@ public class Board {
 	private Piece[][] pieces;
 	
 	public Board(int rows, int columns) {
-		super();
+		if(rows < 1 || columns < 1) {
+			throw new BoardException("Error creating board: there must be at leat 1 row and 1 column");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -16,27 +18,46 @@ public class Board {
 	public int getRows() {
 		return rows;
 	}
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
 	public int getColumns() {
 		return columns;
 	}
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
 	
 	public Piece piece(int row, int column) {
+		if(!positionExists(row, column)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[row][column];
 	}
 	//sobrecarga
 	public Piece piece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[position.getRow()][position.getcolumn()];
 	} 
 	
 	public void placePiece(Piece piece , Position position) {
+		if(thereIsAPiece(position)) {
+			throw new BoardException("There is already a piece on position"+ position);
+		}
+		
 		pieces[position.getRow()][position.getcolumn()] = piece;
 		//consegue acessar livremente protect pq ta no memso pacote
 		piece.position = position;
+	}
+	
+	public boolean positionExists(int row, int column) {
+		return row >= 0 && row< rows && column>=0 && column < columns;
+	}
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getcolumn());
+	}
+	
+	public boolean thereIsAPiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		return piece(position) != null;
 	}
 }
---
+++ src/boadrgame/BoardException.java
@@ -0,0 +1,14 @@
+package boadrgame;
+
+public class BoardException extends RuntimeException {
+
+	/**
+	 * num,ero de versão
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public BoardException(String msg) {
+		super(msg);
+	}
+	
+}
