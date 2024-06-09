9158859781a4f7ac2d5e43e5158717d28cac5bf3
wwpedro <pedroaugusto.ifal@gmail.com>
2024-06-09 10:32:07 -0300

    Implementing possible moves of King

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boadrgame.Board;
+import boadrgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -19,7 +20,59 @@ public class King extends ChessPiece{
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		//testando direções
+		Position p = new Position(0, 0);
+		
+		//a cima
+		p.setValues(position.getRow()-1, position.getcolumn());
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getcolumn()]=true;
+		}
+		//baixo
+		p.setValues(position.getRow()+1, position.getcolumn());
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getcolumn()]=true;
+		}
+		//esquerda
+		p.setValues(position.getRow(), position.getcolumn()-1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getcolumn()]=true;
+		}
+		//diretia
+		p.setValues(position.getRow(), position.getcolumn()+1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getcolumn()]=true;
+		}
+		//diagonal(noroeste)
+		p.setValues(position.getRow()-1, position.getcolumn()-1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getcolumn()]=true;
+		}
+		//diagonal(nordeste)
+		p.setValues(position.getRow()-1, position.getcolumn()+1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getcolumn()]=true;
+		}
+		//diagonal(sudoeste)
+		p.setValues(position.getRow()+1, position.getcolumn()-1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getcolumn()]=true;
+		}
+		//diagonal(suldeste)
+		p.setValues(position.getRow()+1, position.getcolumn()+1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getcolumn()]=true;
+		}
+		
+		
 		return mat;
+	}
+	
+	//regra para mover rei
+	private boolean canMove(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p == null || p.getColor() != getColor();	
 	}
 	
 }
